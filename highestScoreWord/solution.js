5f1b6c2b10a7ec44d10a022c08f073ad58e133d8
Ashabrai Frauen <ashabraifrauen@yahoo.com>
2018-08-29 10:54:28 -0700

    fixed code

--- highestScoreWord/solution.js
+++ highestScoreWord/solution.js
@@ -1,10 +1,10 @@
 
 function high(x){
-    let alphabet = [‘’, ‘a’, ‘b’, ‘c’, ‘d’, ‘e’, ‘f’, ‘g’, ‘h’, ‘i’, ‘j’, ‘k’, ‘l’, ‘m’, ‘n’, ‘o’, ‘p’, ‘q’, ‘r’, ‘s’, ‘t’, ‘u’, ‘v’, ‘w’, ‘x’, ‘y’, ‘z’]
-    let test = x.split(' ’);
+    let alphabet = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    let test = x.split(' ');
     let currentScore = 0;
     let highScore = 0;
-    let highest = “”;
+    let highest = "";
     for (let i = 0; i < test.length; i ++) {
       for (let j = 0; j < test[i].length; j++) {
         for (let k = 0; k < alphabet.length; k++) {
@@ -22,8 +22,7 @@ function high(x){
       }
     }
     return highest;
-   }
- 
+  }
 //another example
 
 // function high(x){
